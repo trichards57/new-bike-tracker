c5776db05be814e99a1d55f20fa2b4033e48560f
Tony Richards <trichards57@gmail.com>
2015-11-21 12:29:53 +0000

    Created initial ReportController.
    Working on #1, #22

---
+++ BikeTracker/Controllers/ReportController.cs
@@ -0,0 +1,48 @@
+﻿using System.Web.Mvc;
+
+namespace BikeTracker.Controllers
+{
+    /// <summary>
+    /// Controller to handle reporting on the activity on the tracker.
+    /// </summary>
+    [Authorize(Roles = "GeneralAdmin")]
+    public class ReportController : Controller
+    {
+        /// <summary>
+        /// Displays the list of reports available to the user
+        /// </summary>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Report that shows all of the LocationRecords for given callsign
+        /// </summary>
+        public ActionResult LocationReport()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Report that shows the reliability measures for a given callsign
+        /// </summary>
+        /// <remarks>
+        /// Measures include:
+        /// * Number of location reports received on time
+        /// * Number of location reports received that did not include a location
+        /// </remarks>
+        public ActionResult ReliabilityReport()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Report that shows all of the activities logged for a given user.
+        /// </summary>
+        public ActionResult UserActivityReport()
+        {
+            return View();
+        }
+    }
+}
