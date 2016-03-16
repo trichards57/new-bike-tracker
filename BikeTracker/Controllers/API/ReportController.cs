d725d9d2f2a277b9b24adb3012d5dad1036a5f1b
Tony Richards <trichards57@gmail.com>
2016-03-16 15:11:10 +0000

    Added report API controller, report models file and report service.

---
+++ BikeTracker/Controllers/API/ReportController.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace BikeTracker.Controllers.API
+{
+    [Authorize(Roles = "GeneralAdmin")]
+    public class ReportController : ApiController
+    {
+        public Task<IHttpActionResult> GetCallsigns()
+        {
+
+        }
+    }
+}
---
+++ BikeTracker/Services/IReportService.cs
@@ -0,0 +1,51 @@
+﻿using BikeTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikeTracker.Services
+{
+    public interface IReportService
+    {
+        Task<IEnumerable<string>> GetAllCallsigns();
+        Task<IEnumerable<LocationRecord>> GetCallsignRecord(string callsign, DateTimeOffset startTime, DateTimeOffset endTime);
+    }
+
+    public class ReportService : IReportService, IDisposable
+    {
+        private ApplicationDbContext dataContext = new ApplicationDbContext();
+
+        public async Task<IEnumerable<string>> GetAllCallsigns()
+        {
+            return await dataContext.LocationRecords.Select(l => l.Callsign).Distinct().ToListAsync();
+        }
+
+        public async Task<IEnumerable<LocationRecord>> GetCallsignRecord(string callsign, DateTimeOffset startTime, DateTimeOffset endTime)
+        {
+            var callsignRecords = dataContext.LocationRecords.Where(l => l.Callsign == callsign && l.ReadingTime >= startTime && l.ReadingTime <= endTime);
+            return await callsignRecords.ToListAsync();
+        }
+
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    dataContext.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+    }
+}
