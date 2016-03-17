5ae902c6039055a5645b1b99e03a27a0946781f2
Tony Richards <trichards57@gmail.com>
2016-03-17 21:57:03 +0000

    Created Tests for ReportController

---
+++ BikeTracker.Tests/Controllers/ReportControllerTests.cs
@@ -0,0 +1,58 @@
+﻿using BikeTracker.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+
+namespace BikeTracker.Tests.Controllers
+{
+    [TestClass]
+    public class ReportControllerTests
+    {
+        [TestMethod]
+        public void Home()
+        {
+            var controller = new ReportController();
+
+            var res = controller.Home();
+
+            var partialView = res as PartialViewResult;
+
+            Assert.IsNotNull(partialView);
+        }
+
+        [TestMethod]
+        public void UserActivity()
+        {
+            var controller = new ReportController();
+
+            var res = controller.UserActivity();
+
+            var partialView = res as PartialViewResult;
+
+            Assert.IsNotNull(partialView);
+        }
+
+        [TestMethod]
+        public void Locations()
+        {
+            var controller = new ReportController();
+
+            var res = controller.Locations();
+
+            var partialView = res as PartialViewResult;
+
+            Assert.IsNotNull(partialView);
+        }
+
+        [TestMethod]
+        public void Rates()
+        {
+            var controller = new ReportController();
+
+            var res = controller.Rates();
+
+            var partialView = res as PartialViewResult;
+
+            Assert.IsNotNull(partialView);
+        }
+    }
+}
