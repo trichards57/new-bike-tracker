93d5e4976686f61f4264171729dd8cdcb703176c
Tony Richards <trichards57@gmail.com>
2016-06-19 11:09:08 +0100

    Updating test tool to support multiple client types.  Defined basic interface for client.

---
+++ BikeTrackerTestTool/Clients/IClient.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace BikeTrackerTestTool.Clients
+{
+    interface IClient
+    {
+        Task<string> SendUpdate();
+        string Imei { get; set; }
+        float BaseLatitude { get; set; }
+        float BaseLongitude { get; set; }
+        double FailureRate { get; set; }
+    }
+}
