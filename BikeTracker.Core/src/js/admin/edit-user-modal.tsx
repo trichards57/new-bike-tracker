ada6b344e332afb8ef515975c080655a8a9b69be
Tony Richards <trichards57@gmail.com>
2017-05-15 05:39:41 +0100

    Updated modals.

---
+++ BikeTracker.Core/src/js/admin/edit-user-modal.tsx
@@ -0,0 +1,22 @@
+﻿import * as React from "react";
+import { Modal } from "reactstrap";
+
+interface IEditUserProps {
+}
+
+interface IEditUserState {
+}
+
+export class EditUserModal extends React.Component<IEditUserProps, IEditUserState> {
+    constructor(props: IEditUserProps) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Modal>
+
+            </Modal>
+        );
+    }
+}
